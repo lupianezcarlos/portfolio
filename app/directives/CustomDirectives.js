479597d835b9eee6ba3ae5f2e22572c0329f674d
cl <cl@razzinteractive.com>
2017-07-31 15:36:47 -0400

    cubes

--- app/directives/CustomDirectives.js
+++ app/directives/CustomDirectives.js
@@ -1,22 +1,41 @@
-angular.module('CustomDirectives',[])
+angular.module('CustomDirectives', [])
 
-.directive('blocksDirective', function($window) {
-      return {
-          restrict:"AE",
-          link : function( scope, element, attrs) {
-            function init() {
-            if($window.innerWidth > 992) {
+    .directive('blocks', function ($window) {
+        return {
+            restrict: "AE",
+            link: function (scope, element, attrs) {
+                function init() {
+                    if ($window.innerWidth > 992) {
                         element.find('.col-lg-4:not(.middle)').css('margin-top', element.find('.middle .textbox').innerHeight());
-                } else {
+                    } else {
                         element.find('.col-lg-4:not(.middle)').removeAttr('style');
+                    }
                 }
+                init();
+
+                angular.element($window).resize(function () {
+                    init();
+                });
+
             }
-          init();
+        }
+    })
 
-           angular.element($window).resize(function() {
-              init();
-           });
 
-          }
-      }
-})
+    .directive('cubeStyle', function ($window, $document) {
+        return {
+            restrict: "AE",
+            link: function (scope, element, attrs) {
+                function init() {
+                    var $cube = element.find('.cube');
+                    var cubeHeight = $cube.innerWidth();
+                    $cube.height(cubeHeight);
+                }
+                init();
+                angular.element($window).resize(function () {
+                    init();
+                })
+
+            }
+        }
+    })
